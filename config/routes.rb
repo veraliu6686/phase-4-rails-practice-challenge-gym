a6287063cee2c35fc6838d3b1d878c76fef5290b
Lianzi <lianziliu.sugar@gmail.com>
2022-12-18 12:38:45 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :memberships, only: [:create]
+  resources :clients, only: [:index, :show, :update]
+  resources :gyms, only: [:index, :show, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
